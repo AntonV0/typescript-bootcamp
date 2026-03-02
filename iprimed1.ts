f7d7874ec9804729e0a50bbbc511aa3966928a4c
AntonV0 <anton.vlasenko94@gmail.com>
2026-03-02 13:32:08 +0000

    Initial commit with typescript intro notes

---
+++ iprimed1.ts
@@ -0,0 +1,46 @@
+// ! -------------------------------------------------------------------------------------------------------
+
+// ! TYPESCRIPT INTRODUCTION:
+
+// ! -------------------------------------------------------------------------------------------------------
+
+// ? What is TypeScript?
+
+// TypeScript is a superset of JavaScript that adds static typing and other features to the language. 
+// It was developed by Microsoft and is designed to help developers write more robust and maintainable code. 
+// TypeScript compiles down to plain JavaScript, which can run in any environment that supports JavaScript,
+// such as web browsers and Node.js.
+
+// TypeScript mainly helps in catching errors at compile time (adds static typing to find errors early), 
+// improving code readability, and providing better tooling support.
+
+// It is commonly used with frameworks like Angular, React, and Vue.js, and is also popular for 
+// server-side development with Node.js.
+
+// ? -------------------------------------------------------------------------------------------------------
+
+// ? Why use TypeScript?
+
+// 1. Static Typing: TypeScript allows you to define types for variables, function parameters, and return
+//    values. This helps catch type-related errors during development, making your code more robust and 
+//    less prone to bugs.
+
+// 2. Improved Tooling: TypeScript provides better tooling support, including autocompletion, type 
+//    checking, and refactoring tools. This can enhance developer productivity and make it easier to 
+//    maintain large codebases.
+
+// 3. Enhanced Code Readability: By explicitly defining types, TypeScript can make your code more
+//    self-documenting and easier to understand for other developers (or even yourself in the future).
+
+// 4. Compatibility with JavaScript: Since TypeScript is a superset of JavaScript, you can gradually
+//    adopt it in your existing JavaScript projects.
+
+// 5. Support for Modern JavaScript Features: TypeScript supports the latest JavaScript features, 
+//    allowing you to use modern syntax and capabilities while still maintaining compatibility with older 
+//    environments.
+
+// 6. Large Community and Ecosystem: TypeScript has a large and active community, which means there are
+//    plenty of resources, libraries, and frameworks that support TypeScript. This can make it easier to
+//    find solutions to problems and integrate with other tools.
+
+// ? -------------------------------------------------------------------------------------------------------
